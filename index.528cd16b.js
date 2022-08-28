c02d254fb39410d5764a446ed19a049882eaa20b
artemborja <105107630+artemborja@users.noreply.github.com>
2022-08-28 16:12:20 +0000

    Deploying to gh-pages from @ artemborja/Team-Working@ce5ad989fadded4ddbc23025f62d3de335579a2f 🚀

--- index.528cd16b.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{container:".reviews-slider",controls:!1,items:3,slideBy:"page",autoplay:!1,navPosition:"bottom",navigation:{nextEl:".btn-right",prevEl:".btn-left"}});
-//# sourceMappingURL=index.528cd16b.js.map
